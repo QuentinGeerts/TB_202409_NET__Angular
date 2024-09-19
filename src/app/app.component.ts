0716e8982e214a575ab3507ae05ca8cff94b2a18
Quentin Geerts <quentin.geerts@bstorm.be>
2024-09-19 09:22:01 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'TB_202409_NET__Angular';
+}
